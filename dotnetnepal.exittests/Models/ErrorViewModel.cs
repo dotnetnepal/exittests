7b4cb60c2ac333bb0bf5adf53bc9e3b2a63969e6
David Silwal <david.silwal@ekbana.net>
2017-11-16 10:01:06 +0545

    Add project files.

---
+++ dotnetnepal.exittests/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetnepal.exittests.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
